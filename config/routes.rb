d5919578573d0e5f365be6b719c0ae50d1762c4e
mukhammadrizooff <legend.it@bk.ru>
2022-06-08 04:33:28 +0500

    Create views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'payments/index'
+  get 'payments/show'
+  get 'groups/index'
+  get 'groups/show'
+  get 'users/index'
+  get 'users/show'
+  
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
