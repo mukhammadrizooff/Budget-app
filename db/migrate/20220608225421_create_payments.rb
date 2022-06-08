b15041ea79f8f34e58906cfed017fcb6e83670bf
mukhammadrizooff <legend.it@bk.ru>
2022-06-09 04:02:24 +0500

    Redo all migrations and update schema

--- db/migrate/20220608225421_create_payments.rb
+++ db/migrate/20220608225421_create_payments.rb
@@ -3,7 +3,7 @@ class CreatePayments < ActiveRecord::Migration[7.0]
     create_table :payments do |t|
       t.string :name
       t.decimal :amount, precision: 10, scale: 2
-
+      
       t.references :author, references: :users, null: false, foreign_key: { to_table: :users }, index: true
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_224808) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_225745) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -51,7 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_224808) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "username"
     t.string "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
